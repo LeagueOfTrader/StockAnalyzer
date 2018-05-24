336f67701682ff8e407ea17d0a17ba3862f79f38
KongfuPanda <7cronaldo@sina.com>
2018-05-24 18:57:03 +0800

    temp
    行业正向筛选

---
+++ StockAnalyzer/DataFilter/IndustryIncludeFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalyzer.DataFilter
+{
+    class IndustryIncludeFilter : StockFilter
+    {
+        List<string> m_specifiedIndustries = new List<string>();
+        public IndustryIncludeFilter(List<string> arr)
+        {
+            m_specifiedIndustries.Clear();
+            m_specifiedIndustries.AddRange(m_specifiedIndustries);
+        }
+
+        public override bool filterMethod(string stockID)
+        {
+            foreach(string industryName in m_specifiedIndustries)
+            {
+                if(IndustryExcludeFilter.isStockInIndustry(stockID, industryName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
